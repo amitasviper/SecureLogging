fc6a3803f59101c38e4910f14de9550f9de098e0
Amit Kumar <amitasviper@gmail.com>
2016-08-07 19:49:16 +0530

    Wireshark log generation

--- sparser.py
+++ sparser.py
@@ -1,5 +1,6 @@
 import pyparsing as pyp
 import itertools
+import pyshark
 
 integer = pyp.Word(pyp.nums)
 ip_addr = pyp.Combine(integer+'.'+integer+'.'+integer+'.'+integer)
@@ -28,4 +29,22 @@ def snort_parse(logfile):
                 fields = bnf.searchString(tmpStr)
                 print(fields)
 
-snort_parse('snort_file')
+def print_conversation_header(pkt):
+    try:
+        protocol =  pkt.transport_layer
+        src_addr = pkt.ip.src
+        src_port = pkt[pkt.transport_layer].srcport
+        dst_addr = pkt.ip.dst
+        dst_port = pkt[pkt.transport_layer].dstport
+        print '%s  %s:%s --> %s:%s' % (protocol, src_addr, src_port, dst_addr, dst_port)
+    except AttributeError as e:
+        #ignore packets that aren't TCP/UDP or IPv4
+        pass
+
+def Initialise():
+    cap = pyshark.LiveCapture(interface='wlan0')
+    cap.apply_on_packets(print_conversation_header, timeout=100)
+
+if __name__ == '__main__':
+    #snort_parse('snort_file')
+    Initialise()
